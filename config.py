d30d3ccb6e811d5b500a89a953ad5cdff21fd861
Adeboye <sdalfriend@gmail.com>
2026-03-25 14:54:37 +0100

    Create config.py

---
+++ config.py
@@ -0,0 +1,11 @@
+import os
+
+TOKEN = os.getenv("8749400865:AAFL5J2HvTn13A6HnuqJQt_38RSGUSaYgqo")
+
+CHANNEL_USERNAME = "@Palmernews"
+
+PLAY_URL = "https://t.me/gemzcoin_bot/playfun?startapp=eyJzIjoidGVsZWdyYW0iLCJtIjoiYm90IiwiZiI6ImJyb2FkY2FzdCJ9"
+
+OFFER_URL = "https://t.me/Bybitglobal_Official_bot/app?startapp=redirect-Wednesday-Airdrop"
+
+SUPPORT_URL = "https://t.me/Palmernews"
